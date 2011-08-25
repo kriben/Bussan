a42f66fb5e4f891f6f9d50ad7794f1adc54852ce
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-08-25 20:46:09 +0200

    Fix to/from town display string (comment was right, code was wrong).

--- src/net/kristian/TrondheimBussesLive/BusStopAdapter.java
+++ src/net/kristian/TrondheimBussesLive/BusStopAdapter.java
@@ -35,15 +35,15 @@ public class BusStopAdapter extends ArrayAdapter<BusStop> {
 		// TODO: refactor this mess
 		// Try to construct a direction string:
 		//   buses with id smaller than 1000 are going to town
-		//   less than eqal 1000 is leaving town
+		//   less than equal 1000 is leaving town
 		//   larger than 2000 is unknown
 		String direction = "";
 		int id = new Integer(busStop.getId()).intValue();
 		if (id < 2000) {
 		    if (id < 1000) 
-		        direction = " (to town)";
-		    else 
 		        direction = " (from town)";
+		    else 
+		        direction = " (to town)";
 		}
 		
 		title.setText(busStop.getName() + direction);
