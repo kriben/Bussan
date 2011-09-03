76cec626a3cb669ce85fd458abd773c878b579cb
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-09-03 21:07:00 +0200

    Add interface for bus list adapters which makes it possible to add to favorites from the FindBusStopByDistanceActivity.

---
+++ src/net/kristian/TrondheimBussesLive/BusStopArrayAdapter.java
@@ -0,0 +1,8 @@
+package net.kristian.TrondheimBussesLive;
+
+import no.kriben.busstopstrondheim.model.BusStop;
+
+public interface BusStopArrayAdapter {
+    
+    public BusStop getBusStop(int position);
+}
